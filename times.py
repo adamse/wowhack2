65530a86c31295ac03f558d06fc7c80525b8d68b
ravertass <ravertass@gmail.com>
2013-08-06 21:24:14 +0200

    Gjorde så att times.py kan printa lite stoff

--- times.py
+++ times.py
@@ -1,4 +1,4 @@
-import simplejson
+import simplejson, pygame
 
 class Times:
   def __init__(self, json="daft_punk.json"):
@@ -13,3 +13,27 @@ class Times:
 
   def sections(self):
     return [t["start"] for t in self.data["sections"]]
+
+def main():
+    times = Times()
+    beats = times.beats()
+    sections = times.sections()
+    bars = times.bars()
+
+    clock = pygame.time.Clock()
+
+    clock.tick()
+
+    song_time = 0
+
+    while 1:
+        song_time += (clock.tick() / float(1000))
+
+        # will crash yeah
+        if (song_time >= beats[0]):
+            sections.pop(0)
+            print("Dance to the section! " + str(beats[0]))
+
+if __name__ == '__main__' : main()
+
+
