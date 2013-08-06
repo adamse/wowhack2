4fd37aaa855c198210282c0294518eaced442fbd
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2013-08-06 20:07:33 +0200

    Get music data from EchoNest & see daft_punk.json

--- music/nest.py
+++ music/nest.py
@@ -1,12 +1,28 @@
-import files
-import play
+import sys
+from subprocess import *
+import os
+import urllib2
+import simplejson
 from pyechonest import config
 from pyechonest import song
 
 config.ECHO_NEST_API_KEY = "CPS7RW8QAUIULBBVJ"
 
-for k, v in files.files.iteritems():
-  songs = song.search(artist = v["artist"], title = v["title"],
-      buckets=["audio_summary"])
-  print k
-  print songs[0].audio_summary
+def main():
+  if len(sys.argv) < 3:
+    print "Usage: " + sys.argv[0] + " ARTIST TITLE"
+    return
+
+  songs = song.search(artist = sys.argv[1], title = sys.argv[2], buckets = ["audio_summary"])
+  data = {}
+
+  print "Matching songs:", len(songs)
+
+  for s in songs:
+    print s
+    data = Popen("curl \"" + s.audio_summary["analysis_url"] + "\"", shell=True, stdout=PIPE).stdout
+    print simplejson.load(data)["bars"]
+    break
+
+if __name__ == "__main__":
+  main()
