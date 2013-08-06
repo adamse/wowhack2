c1df2bb07fc2144014b785f672b4da57160aab83
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2013-08-06 21:03:19 +0200

    f is NOT undefined

--- times.py
+++ times.py
@@ -2,14 +2,14 @@ import simplejson
 
 class Times:
   def __init__(self, json="daft_punk.json"):
-    with open(json):
+    with open(json) as f:
       self.data = simplejson.load(f)
 
-  def bars():
+  def bars(self):
     return [t["start"] for t in self.data["bars"]]
 
-  def beats():
+  def beats(self):
     return [t["start"] for t in self.data["beats"]]
 
-  def sections():
+  def sections(self):
     return [t["start"] for t in self.data["sections"]]
