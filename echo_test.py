aa94ca99d9d86ea6b3c0dd83089fe99bd9880946
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2013-08-06 13:59:46 +0200

    pyspotify

--- echo_test.py
+++ echo_test.py
@@ -1,7 +1,11 @@
 import urllib2
 import simplejson
 
-req = urllib2.Request("http://developer.echonest.com/api/v4/artist/search?api_key=1ZZLRKF6Z9IN6OBUI&name=suicide")
+k = "1ZZLRKF6Z9IN6OBUI"
+
+req =urllib2.Request(
+"http://developer.echonest.com/api/v4/song/profile?api_key="
+    + k + "&id=spotify:track:0MYTcPXAAmeKBUpBgtAV0J")
 opener = urllib2.build_opener()
 f = opener.open(req)
 suicide = simplejson.load(f)
