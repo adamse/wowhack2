02ee518d6810b9918cafb02b96c929108f8e9c31
majstroll <nithander@gmail.com>
2013-08-06 17:39:16 +0200

    heman says heyeyeyeyeyeyey Whats goin on?

--- letsgobananas.py
+++ letsgobananas.py
@@ -10,30 +10,38 @@ class Lemon(something imagy)
 	def __init__(self):
 	def lemonparty(i):
 """
+size = width, height = 640, 480
+screen = pygame.display.set_mode(size)
+
+class Fruit:
+    def __init__(self, name, speed = 1):
+        ## Load the image and get the rect from the image
+        self.name = name
+        self.image = pygame.image.load(os.path.join('bilder', name + ".png"))
+        self.image = pygame.transform.scale(self.image,(50,50))         
+        self.rect = self.image.get_rect()
+        
+        ## The speed of which the fruit will fall
+        self.speed = speed
+
+    ## Blits the fruit to the screen
+    def blit(self):
+        screen.blit(self.image, self.rect)
+
+    ## Moves the fruit according to the set speed
+    def move(self):
+        self.rect = self.rect.move((0, self.speed))
+
 def main():
-    speed = [0,-1]
-    size = width, height = 640, 480
     black = 0, 0, 0
-    screen = pygame.display.set_mode(size)
-	
-    lemon = pygame.image.load(os.path.join('bilder','lemon.png'))
-    lemon = pygame.transform.scale(lemon,(50,50))	
-    lemonrect = lemon.get_rect()
-
-    apple = pygame.image.load(os.path.join('bilder','apple.png'))
-    apple = pygame.transform.scale(apple,(50,50))	
-    applerect = apple.get_rect()
-
-    banana = pygame.image.load(os.path.join('bilder','banana.png'))
-    banana = pygame.transform.scale(banana,(50,50))
-    bananarect = banana.get_rect()
-	
     pygame.init()
-    fruitdir = {K_UP: ('banana',banana,bananarect),K_LEFT: ('lemon',lemon,lemonrect), K_RIGHT: ('apple',apple,applerect) }
+    fruitdir = {K_UP: ('banana'),K_LEFT: ('lemon'), K_RIGHT: ('apple') }
     currentFruit = None    
     currentRect = None
     currentImg = None
+    fruits = []
     while 1:
+        screen.fill(black)
         for event in pygame.event.get():
             if event.type == QUIT: return
             elif event.type == KEYDOWN:
@@ -41,22 +49,22 @@ def main():
                 if button == K_ESCAPE: return			
                 elif button in (K_UP,K_LEFT,K_RIGHT):
                     name = pygame.key.name(button)
-                    (currentFruit,currentImg,currentRect) = fruitdir[button]
-                    screen.fill(black)
-                    screen.blit(currentImg,currentRect)
+                    currentFruit = fruitdir[button]
+                    new_fruit = Fruit(currentFruit)
+                    fruits.append(new_fruit)                    
+                    new_fruit.move()
                     pygame.display.flip()
                     print 'Well played, you pressed the ' + name + ' button!'					
                     print 'A wild ' + currentFruit + ' appeared!'
                 else:
 					print 'You fail!'
-        if currentFruit is not None:
-            currentRect = currentRect.move(speed)
-            screen.fill(black)
-            screen.blit(currentImg,currentRect)				
-            pygame.display.flip()
-            if currentRect.top < 0: 
-                speed[1] = -speed[1]                
-            elif currentRect.bottom > height:
-                speed[1] = -speed[1]
-                print 'An unfortunate ' + currentFruit + ' dropped to the ground!'				
+        for f in fruits :
+            f.move()
+            f.blit()				
+            if f.rect.top < 0: 
+                f.speed = - f.speed                
+            elif f.rect.bottom > height:
+                f.speed = - f.speed
+                print 'An unfortunate ' + f.name + ' fell to the ground!'
+        pygame.display.flip()				
 if __name__ == '__main__': main()
